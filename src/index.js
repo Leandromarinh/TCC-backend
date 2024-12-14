0db77bae8e61948cdbfb6da54446133e64ea9d7d
Leandromarinh <leandrinm14@gmail.com>
2024-12-14 19:13:55 -0300

    adding mongo bundle cert

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const fs = require("fs");
 
 const AuthRoute = require("./routes/auth");
 const UserRoute = require("./routes/user");
@@ -10,10 +11,18 @@ const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 const PORT = process.env.PORT || 3200;
 const origin = process.env.ORIGIN;
+const cert = process.env.CERT
+
+const options = {
+  ssl: true,
+  sslValidate: true,
+  sslCA: fs.readFileSync(cert),
+};
 
 mongoose
   .connect(
-    `mongodb://${dbUser}:${dbPass}@docdb-tcc.cluster-covg2ow5gv7z.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`
+    `mongodb://${dbUser}:${dbPass}@docdb-tcc.cluster-covg2ow5gv7z.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`,
+    options
   )
   .then(() => {
     console.log("Conectou ao banco!");
